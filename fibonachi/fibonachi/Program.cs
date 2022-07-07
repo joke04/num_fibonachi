ddf738a2496dcf7479019b4c2de53af93ad15fef
joke04 <108774312+joke04@users.noreply.github.com>
2022-07-07 14:22:02 +0300

    Add files via upload

---
+++ fibonachi/fibonachi/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("До какого числа вывести ряд Фибоначчи? ");
+int final = Convert.ToInt32(Console.ReadLine());
+
+int first = 1; int second = 1;
+Console.Write("{0} ", first, "{1} ", second);
+int sum = 0;
+
+while (final >= sum)
+{
+    sum = first + second;
+    Console.Write("{0} ", sum);
+    first = second;
+    second = sum;
+}
